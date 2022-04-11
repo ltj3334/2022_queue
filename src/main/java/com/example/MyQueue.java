50db85da70b12aa992c7f0a8d1d4c6beb6939fd5
ltj3334 <ltj3334@naver.com>
2022-04-11 09:57:28 +0900

    add interface file

---
+++ src/main/java/com/example/MyQueue.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public interface MyQueue {
+    // int max = 100;
+    // int[] queue = new int[max];
+    // int front = 0;
+    // int rear = 0;
+    
+    // 큐 push
+    public void push(int x);
+    
+    // 큐 pop
+    public int pop();
+    
+    // 큐 비었는지 확인
+    public boolean isEmpty();
+
+    // 큐 비었는지 확인
+    public boolean isFull();
+}
